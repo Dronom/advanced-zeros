3982621475e1474d3a855167ba4418b1d5759212
Roman <dronymys@mail.ru>
2019-03-17 13:10:22 +0300

    make advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const decompBase = [];
+  decompBase[base] = 0;
+  decompBase.fill(0);
+  let zeros = 0;
+  let i = 2;
+
+  for(let tempBase = base; tempBase>1; i++){
+    while(tempBase % i == 0){
+      decompBase[i]++;
+      tempBase /= i;
+    }
+  }
+
+  decompBase.splice(i,decompBase.length);
+
+  for(i = 2; i < decompBase.length; i++){
+    if(decompBase[i] > 0){
+      let power = 1;
+      while((number/Math.pow(i,power)) > 1){
+        zeros += Math.floor(number / (Math.pow(i,power)));
+        power++;
+      }
+      zeros = Math.floor(zeros / decompBase[i]);
+      i++;
+      break;
+    }
+  }
+
+
+  for(let temp = 0; i < decompBase.length; i++){
+    if(decompBase[i]>0){
+      temp = 0;
+      for(let power = 1; power<=decompBase[i],(number / Math.pow(i,power)) > 1 ; power++){
+        temp += Math.floor(number/Math.pow(i,power));
+      }
+      temp = Math.floor(temp / decompBase[i]);
+      if(temp < zeros) zeros = temp;
+    }
+    
+  }
+
+  return zeros;
 }
